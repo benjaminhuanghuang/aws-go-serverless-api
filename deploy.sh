aa958321ebd7b21c912fccc60425464d6e815037
Benjamin Huang <bhuang@rms.com>
2020-01-12 19:22:53 -0800

    update

--- deploy.sh
+++ deploy.sh
@@ -4,3 +4,5 @@ env GOOS=linux GOARCH=amd64 go build -o ./temp/main ./books
 zip -j ./temp/main.zip ./temp/main
 
 
+aws lambda update-function-code --function-name books \
+--zip-file fileb://./temp/main.zip
